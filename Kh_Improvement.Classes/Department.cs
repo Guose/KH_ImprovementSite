c4af3fdd9513361955709db37a907f6bec0bcb3a
Guose <guose79@gmail.com>
2016-03-14 09:19:08 -0700

    Update Classes 3-14

---
+++ Kh_Improvement.Classes/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kh_Improvement.Classes
+{
+    public class Department
+    {
+        public int DeptId { get; set; }
+        public string DeptName { get; set; }
+        public string DeptHead { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+}
---
+++ Kh_Improvement.Classes/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kh_Improvement.Classes
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public Department Department { get; set; }
+        public int DeptId { get; set; }
+    }
+}
